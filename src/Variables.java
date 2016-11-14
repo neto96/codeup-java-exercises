3f780031031684ee03ca80d3222844554c3aaa1f
Ernesto <neto_neto@hotmail.com>
2016-11-14 10:27:47 -0600

    Simple variables exercise

---
+++ src/Variables.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Neto on 11/11/16.
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        byte DaByte = 3;
+        System.out.println(DaByte);
+        short DaShort = 32767;
+        System.out.println(DaShort);
+        int DaInt = 87607607;
+        System.out.println(DaInt);
+        long DaLong = 922330986;
+        System.out.println(DaLong);
+        float DaFloat = 333333333;
+        System.out.println(DaFloat);
+        double DaDouble = 3.2345465784547878979469769679;
+        System.out.println(DaDouble);
+        char DaChar = 'c';
+        System.out.println(DaChar);
+        boolean DaBool = false;
+        System.out.println(DaBool);
+    }
+}

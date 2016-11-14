a70ec7acaf5266373671ba87f0bdb54e8c45fa00
Ernesto <neto_neto@hotmail.com>
2016-11-14 13:45:32 -0600

    Strings exercise using StringBuffer

---
+++ src/Strings.java
@@ -0,0 +1,16 @@
+
+/**
+ * Created by Neto on 11/14/16.
+ */
+public class Strings {
+    public static void main(String[] args) {
+        StringBuffer message = new StringBuffer("");
+        message.append("We don't need no education \n");
+        message.append("We don't need no thought control \n");
+        message.append("Check \"this\" out!, \"s inside of \"s! \n");
+        message.append("In windows, the main drive is usually C:\\ \n");
+        message.append("I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\!\n");
+        System.out.println(message);
+
+    }
+}
